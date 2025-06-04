ea1f29081a15dcc446cb78d54bad80f290369a88
SuriyaPasupathi <suriyasuri.481@gmail.com>
2025-06-04 07:00:23 -0700

    docker added

--- proven_pro/proven_pro/settings.py
+++ proven_pro/proven_pro/settings.py
@@ -137,6 +137,9 @@ DATABASES = {
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
