2a2fb14fed969c80dfd49c5efe1ff4b6b1e5130f
SuriyaPasupathi <suriyasuri.481@gmail.com>
2025-06-25 11:43:25 +0530

    plan details final format

--- proven_pro/api/models.py
+++ proven_pro/api/models.py
@@ -306,10 +306,9 @@ class PlanDetails(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     plan_name = models.CharField(max_length=20, choices=PLAN_CHOICES, unique=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    includes = models.TextField()  # JSON will be stored as text string
+    includes = models.TextField()
 
     def __str__(self):
         return f"{self.get_plan_name_display()} - ${self.price}"
-
 
 
