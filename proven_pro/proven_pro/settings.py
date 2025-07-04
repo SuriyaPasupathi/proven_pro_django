6e1c5e142fd7ff360207a21b398695219de7757c
SuriyaPasupathi <suriyasuri.481@gmail.com>
2025-07-04 15:43:06 +0530

    view add it

--- proven_pro/proven_pro/settings.py
+++ proven_pro/proven_pro/settings.py
@@ -164,8 +164,8 @@ DATABASES = {
         'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST', default='db1'),
-        'PORT': config('DB_PORT', default='3306'),
+        'HOST': config('DB_HOST','db'),
+        'PORT': config('DB_PORT','3306'),
     }
 }
 
